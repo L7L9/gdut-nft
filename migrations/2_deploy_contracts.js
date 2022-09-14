5989b96d743dffda1c2d488ea5de74ef8a2dd48a
lql <1964757607@qq.com>
2022-09-14 22:23:57 +0800

    feat:新增部署活动合约的指令

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Factory = artifacts.require("Factory");
+const Activity = artifacts.require("Activity");
 
 module.exports = function(deployer) {
   deployer.deploy(Factory);
+  deployer.deploy(Activity);
 };
