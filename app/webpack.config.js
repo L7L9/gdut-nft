55be291f0d2674d819eca430b79d8fd94416877c
lql <1964757607@qq.com>
2022-09-17 18:32:13 +0800

    feat:添加显示活动的页面

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
     new CopyWebpackPlugin([{ from: "./src/home.html", to: "home.html" }]),
     new CopyWebpackPlugin([{ from: "./src/create.html", to: "create.html" }]),
     new CopyWebpackPlugin([{ from: "./src/myInformation.html", to: "myInformation.html" }]),
-    new CopyWebpackPlugin([{ from: "./src/initiateActivity.html", to: "initiateActivity.html" }]),
     new CopyWebpackPlugin([{ from: "./src/activity.html", to: "activity.html" }]),
+    new CopyWebpackPlugin([{ from: "./src/initiateActivity.html", to: "initiateActivity.html" }]),
   ],
   devServer: { 
     contentBase: path.join(__dirname, "dist"),
